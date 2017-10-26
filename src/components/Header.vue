b7cd6b3ef29984586812d484ce5d4a007d4f4868
onluiz <luiz.alberto1723@gmail.com>
2017-10-26 18:32:19 -0200

    adicionando algumas coisas

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        title: 'vuetify template'
+      }
+    }
+  }
+</script>
---
+++ src/components/ListCards.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+      <v-content>
+        <v-container fluid>
+            <div v-for="card in cards">
+                    <Card />
+                </div>
+        </v-container>
+      </v-content>
+    </main>
+</template>
+
+<script>
+import Card from './Card'
+
+export default {
+  components: {
+    Card
+  },
+  data () {
+    return {
+      cards: [{
+        id: 1,
+        title: 'Título'
+      }]
+    }
+  }
+}
+</script>
